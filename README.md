16ea83f926517e55625bb19554d7a4fa59a06cb3
Justin Lee <leejl950515@gmail.com>
2020-09-09 20:45:51 +0000

    Added export & download csv file functionality

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 A web scrapper to search for job positions in indeed.ca
 
-Implemented with and pushed from `repl.it` 
+Implemented with and pushed from `repl.it`
+
+Link: https://repl.it/@JustinLee2/JobScraper
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from indeed_scrapper import get_indeed_jobs
 
 app = Flask("Job Scraper")
@@ -10,10 +10,19 @@ def save_to_file(jobs):
   writer = csv.writer(file)
   writer.writerow(["Title", "Company", "Location", "Link"])
 
+  print("Writing...")
   for i in range(len(jobs)):
     writer.writerow(list(jobs[i].values()))
+  print("Successfully finished saving a file")
 
-  return None
+  return
+
+def fetch_from_cache(keyword):
+  fromDb = db.get(keyword)
+  if fromDb is not None:
+    return fromDb
+  else:
+    return None
 
 @app.route("/")
 def home():
@@ -24,19 +33,33 @@ def report():
   word = request.args.get('word')
   if word is not None:
     word = word.lower()
-    fromDb = db.get(word)
-    if fromDb is not None:
-      jobs = fromDb
-    else:
+    jobs = fetch_from_cache(word)
+    if jobs is None:
       jobs = get_indeed_jobs(word)
       db[word] = jobs
-    save_to_file(jobs)
     return render_template("report.html",
       search=word,
       resultsLen=len(jobs),
       jobs=jobs
     )
   else:
+    return redirect("/")
+
+@app.route("/export")
+def export():
+  try:
+    word = request.args.get('word')
+    if word is not None:
+      word = word.lower()
+      jobs = fetch_from_cache(word)
+      if jobs is None:
+        raise Exception()
+      save_to_file(jobs)
+      return send_file("jobs.csv", as_attachment=True)
+    else:
+      raise Exception()
+
+  except:
     return redirect("/")
 
 app.run(host="0.0.0.0")
